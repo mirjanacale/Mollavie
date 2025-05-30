2dd79517d1765d524361032455707bc0351ce5f4
MirjanaCale1 <mirjana083@gmail.com>
2025-05-30 15:53:22 +0100

    Adding fixsed image

--- mollavie/settings.py
+++ mollavie/settings.py
@@ -21,7 +21,7 @@ if os.path.exists(env_path):
 
 #  Secret Key
 SECRET_KEY = os.environ.get('SECRET_KEY') or 'temporary-insecure-key'
-DEBUG = os.environ.get('DEBUG') == 'True'
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = ["*", "localhost", "127.0.0.1", ".herokuapp.com"]
 
 
